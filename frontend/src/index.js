34d9ac14476483ab5bc2058bd238f678e50c76b9
nikolasokolov <35638588+nikolasokolov@users.noreply.github.com>
2018-11-28 09:20:10 +0100

    Updated config for redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,7 @@ import authReducer from './store/reducers/auth';
 import 'font-awesome/css/font-awesome.min.css';
 
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
 
 const store = createStore(authReducer, composeEnhancers(applyMiddleware(thunk)));
 
